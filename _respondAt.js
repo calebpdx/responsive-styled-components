c5a954d46f5cdf85cd912b68f48f6c362fa213f8
Caleb White <caleb@fairwaycreative.com>
2021-07-07 00:00:23 -0700

    small update to the export

--- _respondAt.js
+++ _respondAt.js
@@ -1,14 +1,13 @@
 import { css } from 'styled-components'
 const breakpoints = require(process.env.INIT_CWD('_variables.js'))
 
-export const respondAt = Object.keys(breakpoints).reduce(
-	(accumulator, label) => {
-		accumulator[label] = (...args) => css`
-			@media (min-width: ${breakpoints[label]}) {
-				${css(...args)};
-			}
-		`
-		return accumulator
-	},
-	{}
-)
+const respondAt = Object.keys(breakpoints).reduce((accumulator, label) => {
+	accumulator[label] = (...args) => css`
+		@media (min-width: ${breakpoints[label]}) {
+			${css(...args)};
+		}
+	`
+	return accumulator
+}, {})
+
+export default respondAt
---
+++ _variables.js
@@ -0,0 +1,7 @@
+export const breakpoints = {
+	xs: '480px',
+	sm: '768px',
+	md: '992px',
+	lg: '1173px',
+	xl: '1920px',
+}
